fe498a25a2978cb63faeff51b0dbc97a8d975cc8
Fiaz Sami <fiaz.sami@gmail.com>
2021-07-21 11:13:49 -0500

    added greeks for call

--- src/options.py
+++ src/options.py
@@ -1,11 +1,11 @@
-from math import log, sqrt, exp
+from math import log, sqrt, exp, pi
 from scipy.stats import norm
 
 class BlackScholes:
 
     @classmethod
-    def d1(cls, s, x, r, sig, t):
-        return (log(s / x) + (r + (sig ** 2) / 2) * t) / (sig * (sqrt(t)))
+    def d1(cls, s, x, r, sig, t, k=0):
+        return (log(s / x) + (r - k + (sig ** 2) / 2) * t) / (sig * (sqrt(t)))
 
     @classmethod
     def d2(cls, d1, sig, t):
@@ -19,3 +19,27 @@ class BlackScholes:
     @classmethod
     def put_price(cls, s, x, r, sig, t):
         return BlackScholes.call_price(s, x, r, sig, t) - s + x * exp(-r * t)
+
+    @classmethod
+    def delta(cls, d1, t, k=0):
+        return exp(-k*t) * norm.cdf(d1)
+
+    @classmethod
+    def gamma(cls, d1, s, sig, t, k=0):
+        return exp((-d1 ** 2) / 2 - k * t) / (s * sig * sqrt(2 * t * pi))
+
+    @classmethod
+    def vega(cls, d1, s, t, k=0):
+        return s * sqrt(t) * exp(-(d1 ** 2) / 2) * exp(-k * t) / sqrt(2 * pi)
+
+    @classmethod
+    def rho(cls, d2, s, x, r, t):
+        return x * t * exp(-r * t) * norm.cdf(d2)
+
+
+    @classmethod
+    def theta(cls, d1, d2, s, x, r, sig, t, k):
+        return -s * exp(-(d1 ** 2) / 2 - k * t) * sig / sqrt(8 * t * pi) + k * s * exp(-k * t) * norm.cdf(d1) - r * x * exp(-r * t) * norm.cdf(d2)
+
+
+
--- tests/test_options.py
+++ tests/test_options.py
@@ -3,8 +3,11 @@ import src.options as sut
 
 class TestOptions(unittest.TestCase):
 
-    def test_d1(self):
+    def test_d1_nodiv(self):
         self.assertEqual(0.6509066782522832, sut.BlackScholes.d1(50., 45., 0.04, 0.3, 0.75))
+
+    def test_d1_div(self):
+        self.assertEqual(0.630276662220168, sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02))
 
     def test_d2(self):
         d1 = sut.BlackScholes.d1(50., 45., 0.04, 0.3, 0.75)
@@ -15,3 +18,25 @@ class TestOptions(unittest.TestCase):
 
     def test_put_price(self):
         self.assertEqual(2.3134827167985392, sut.BlackScholes.put_price(50., 45., 0.04, 0.3, 0.75))
+
+    def test_delta(self):
+        d1 = sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02)
+        self.assertEqual(0.728422438120952, sut.BlackScholes.delta(d1, 0.5, 0.02))
+
+    def test_gamma(self):
+        d1 = sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02)
+        self.assertEqual(0.013084364125226582, sut.BlackScholes.gamma(d1, 100, 0.35, 0.5, 0.02))
+
+    def test_vega(self):
+        d1 = sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02)
+        self.assertEqual(22.897637219146517, sut.BlackScholes.vega(d1, 100, 0.5, 0.02))
+
+    def test_rho(self):
+        d1 = sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02)
+        d2 = sut.BlackScholes.d2(d1, 0.35, 0.5)
+        self.assertEqual(28.34456926246856, sut.BlackScholes.rho(d2, 100, 90, 0.06, 0.5))
+
+    def test_theta(self):
+        d1 = sut.BlackScholes.d1(100, 90, 0.06, 0.35, 0.5, 0.02)
+        d2 = sut.BlackScholes.d2(d1, 0.35, 0.5)
+        self.assertEqual(-9.958676461955603, sut.BlackScholes.theta(d1, d2, 100, 90, 0.06, 0.35, 0.5, 0.02))
